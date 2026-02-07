11bb99225f48aa1e1c41d4d0170f8b8a81b71783
Mykhailo Doronin <mykhailo.doronin@kyivcity.gov.ua>
2026-02-07 22:55:28 +0200

    some config changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,12 @@ plugins {
 
 android {
     namespace = "com.dormiwww.echodo"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.dormiwww.echodo"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
